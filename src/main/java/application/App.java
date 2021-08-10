a458f39b115b1b56bb66b15a1ab01e78e204d7a3
Ben Shabowski <benshabowski@gmail.com>
2021-08-09 23:26:28 -0500

    Doing my best I guess

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -25,18 +25,21 @@ public class App {
 	private static Properties injectProperties() {
 		Properties properties = new Properties();
 		properties.setProperty("spring.main.banner-mode", "off");
+		
 		properties.setProperty("spring.http.multipart.enabled", "false");
 		properties.setProperty("spring.http.multipart.maxFileSize", "-1");
 		properties.setProperty("spring.http.multipart.maxRequestSize", "-1");
+		
 		properties.setProperty("logging.level.root", "INFO");
 		properties.setProperty("server.port", config.getPort());
+		
 		if(!config.getKeystoreLocation().equals("none")) {
-			// secure this bad boi
 			properties.setProperty("server.ssl.enabled", "true");
 			properties.setProperty("server.ssl.key-store", config.getKeystoreLocation());
 			properties.setProperty("server.ssl.key-alias", "tomcat");
 			properties.setProperty("server.ssl.key-store-password", config.getKeystorePassword());
 		}
+		
 		return properties;
 	}
 	
