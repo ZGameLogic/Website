69b72df45bac6889fefc769af86d6e1cc8a62951
Ben Shabowski <benshabowski@gmail.com>
2021-08-09 20:00:51 -0500

    Adding file down-loader

---
+++ src/main/java/controllers/FileDownloadController.java
@@ -0,0 +1,71 @@
+package controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class FileDownloadController {
+
+	private File downloadFolder;
+
+	@PostConstruct
+	public void initialize() {
+		downloadFolder = new File("downloads");
+		if (!downloadFolder.exists()) {
+			downloadFolder.mkdirs();
+		}
+	}
+
+	@GetMapping("/downloads/{fileName:.+}")
+	public void download(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("fileName") String fileName) throws IOException {
+		// get absolute path of the application
+		ServletContext context = request.getServletContext();
+
+		// construct the complete absolute path of the file
+		String fullPath = downloadFolder + "\\" + fileName;
+		File downloadFile = new File(fullPath);
+		FileInputStream inputStream = new FileInputStream(downloadFile);
+
+		// get MIME type of the file
+		String mimeType = context.getMimeType(fullPath);
+		if (mimeType == null) {
+			// set to binary type if MIME mapping not found
+			mimeType = "application/octet-stream";
+		}
+
+		// set content attributes for the response
+		response.setContentType(mimeType);
+		response.setContentLength((int) downloadFile.length());
+
+		// set headers for the response
+		String headerKey = "Content-Disposition";
+		String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+		response.setHeader(headerKey, headerValue);
+
+		// get output stream of the response
+		OutputStream outStream = response.getOutputStream();
+
+		byte[] buffer = new byte[4096];
+		int bytesRead = -1;
+
+		// write bytes read from the input stream into the output stream
+		while ((bytesRead = inputStream.read(buffer)) != -1) {
+			outStream.write(buffer, 0, bytesRead);
+		}
+
+		inputStream.close();
+		outStream.close();
+	}
+}
