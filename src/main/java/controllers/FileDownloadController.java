6b2adf2ad4e910582737a337605469b4deaebbf1
Ben Shabowski <benshabowski@gmail.com>
2021-08-10 12:31:06 -0500

    Large file download fix? WEB-4

--- src/main/java/controllers/FileDownloadController.java
+++ src/main/java/controllers/FileDownloadController.java
@@ -1,13 +1,17 @@
 package controllers;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,18 +29,31 @@ public class FileDownloadController {
 		}
 	}
 
+	
 	@GetMapping("/downloads/{fileName:.+}")
-	public void download(HttpServletResponse response,
-			@PathVariable("fileName") String fileName) throws IOException {
-		try {
-			// get your file as InputStream
-			InputStream is = new FileInputStream(new File(downloadFolder + "\\" + fileName));
-			// copy it to response's OutputStream
-			org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
-			response.flushBuffer();
-		} catch (IOException ex) {
-			System.out.println("bad");
-			ex.printStackTrace();
-		}
+	public ResponseEntity<Resource> download(HttpServletResponse response, HttpServletRequest request, @PathVariable("fileName") String fileName) throws IOException {
+		
+		// Load file as Resource
+        Resource resource = new UrlResource("file:" + downloadFolder + "//" + fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+        	
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    
 	}
+	
 }
