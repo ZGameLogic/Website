ef7a7e54ebc6266bfb3bddf1639f42d3483b01cc
Ben Shabowski <benshabowski@gmail.com>
2021-08-09 23:08:21 -0500

    hopefully fixing file size issue

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -25,6 +25,9 @@ public class App {
 	private static Properties injectProperties() {
 		Properties properties = new Properties();
 		properties.setProperty("spring.main.banner-mode", "off");
+		properties.setProperty("spring.http.multipart.enabled", "false");
+		properties.setProperty("spring.http.multipart.maxFileSize", "-1");
+		properties.setProperty("spring.http.multipart.maxRequestSize", "-1");
 		properties.setProperty("logging.level.root", "INFO");
 		properties.setProperty("server.port", config.getPort());
 		if(!config.getKeystoreLocation().equals("none")) {
--- src/main/java/controllers/FileDownloadController.java
+++ src/main/java/controllers/FileDownloadController.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
@@ -27,60 +26,16 @@ public class FileDownloadController {
 	}
 
 	@GetMapping("/downloads/{fileName:.+}")
-	public void download(HttpServletRequest request, HttpServletResponse response,
+	public void download(HttpServletResponse response,
 			@PathVariable("fileName") String fileName) throws IOException {
-		 try {
-		      // get your file as InputStream
-		      InputStream is = new FileInputStream(new File(downloadFolder + "\\" + fileName));
-		      // copy it to response's OutputStream
-		      org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
-		      response.flushBuffer();
-		    } catch (IOException ex) {
-		    	System.out.println("bad");
-		    }
-	}
-	
-	/*
-	@GetMapping("/downloads/{fileName:.+}")
-	public void download(HttpServletRequest request, HttpServletResponse response,
-			@PathVariable("fileName") String fileName) throws IOException {
-		// get absolute path of the application
-		ServletContext context = request.getServletContext();
-
-		// construct the complete absolute path of the file
-		String fullPath = downloadFolder + "\\" + fileName;
-		File downloadFile = new File(fullPath);
-		FileInputStream inputStream = new FileInputStream(downloadFile);
-
-		// get MIME type of the file
-		String mimeType = context.getMimeType(fullPath);
-		if (mimeType == null) {
-			// set to binary type if MIME mapping not found
-			mimeType = "application/octet-stream";
+		try {
+			// get your file as InputStream
+			InputStream is = new FileInputStream(new File(downloadFolder + "\\" + fileName));
+			// copy it to response's OutputStream
+			org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+			response.flushBuffer();
+		} catch (IOException ex) {
+			System.out.println("bad");
 		}
-
-		// set content attributes for the response
-		response.setContentType(mimeType);
-		response.setContentLength((int) downloadFile.length());
-
-		// set headers for the response
-		String headerKey = "Content-Disposition";
-		String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
-		response.setHeader(headerKey, headerValue);
-
-		// get output stream of the response
-		OutputStream outStream = response.getOutputStream();
-
-		byte[] buffer = new byte[4096];
-		int bytesRead = -1;
-
-		// write bytes read from the input stream into the output stream
-		while ((bytesRead = inputStream.read(buffer)) != -1) {
-			outStream.write(buffer, 0, bytesRead);
-		}
-
-		inputStream.close();
-		outStream.close();
 	}
-	*/
 }
