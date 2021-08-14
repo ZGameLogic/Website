b7cf51651f44a1bb9ef249c1b6fb6a1758d46f32
Ben Shabowski <benshabowski@gmail.com>
2021-08-14 13:13:22 -0500

    Changing size of website.info in the database

--- src/main/java/dataStructures/Project.java
+++ src/main/java/dataStructures/Project.java
@@ -3,6 +3,7 @@ package dataStructures;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -31,6 +32,7 @@ public class Project {
 	private String name;
 	private String description;
 	private String url;
+	@Column(name = "website_info", length = 1000)
 	private String websiteInfo;
 	
 	public Project() {
