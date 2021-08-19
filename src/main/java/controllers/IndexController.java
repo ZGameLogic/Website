34acbeef292a1fb22b708a4c132bbb46e784682a
Ben Shabowski <benshabowski@gmail.com>
2021-08-19 10:40:36 -0500

    Adding a contact me section to the index page

--- src/main/java/controllers/IndexController.java
+++ src/main/java/controllers/IndexController.java
@@ -1,56 +1,38 @@
 package controllers;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
-
+	
 	@Autowired
-	private RequestMappingHandlerMapping re;
+    private JavaMailSender emailSender;
 	
 	@GetMapping({ "/", "/index" })
 	public String main() {
 		return "index";
 	}
 
-	@GetMapping("/sitemap.xml")
-	public void getSitemap(HttpServletResponse response) {
-		response.setContentType("application/xml");
-		Map<RequestMappingInfo, HandlerMethod> handlerMethods = re.getHandlerMethods();
-		List<String> urls = new ArrayList<>();
-		for (Entry<RequestMappingInfo, HandlerMethod> entry : handlerMethods.entrySet()) {
-			urls.addAll((entry.getKey().getPatternsCondition().getPatterns()));
-		}
+	@PostMapping("/response")
+	public String response(
+			@RequestParam("from") String from,
+			@RequestParam("subject") String subject,
+			@RequestParam("body") String body) {
 		
-		// Construct XML response from urls and return it
-		for(String x : urls) {
-			try {
-				if(!x.equals("/")) {
-					System.out.println(x);
-					response.getOutputStream().println(x);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		try {
-			response.getOutputStream().flush();
-			response.getOutputStream().close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		SimpleMailMessage message = new SimpleMailMessage(); 
+        message.setFrom(from);
+        message.setTo("ben@zgamelogic.com"); 
+        message.setSubject(subject); 
+        message.setText(body + "\nThis was submitted via the website");
+        emailSender.send(message);
+		
+		return "response";
 	}
+	
 }
