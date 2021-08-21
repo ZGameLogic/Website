23b1c7e474d3835680e6bf20acf46de3953d493b
Ben Shabowski <benshabowski@gmail.com>
2021-08-21 00:39:01 -0500

    Stupid space

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 import data.ConfigLoader;
 import lombok.Getter;
 
