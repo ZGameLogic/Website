d3d4bfc8b50d55f1457dee0882a82230d06ff848
Ben Shabowski <benshabowski@gmail.com>
2021-09-14 22:22:08 -0500

    Adding the ability to edit reservations

--- src/main/java/controllers/ReservationController.java
+++ src/main/java/controllers/ReservationController.java
@@ -38,9 +38,54 @@ public class ReservationController {
 	
 	@GetMapping("/reservations/edit")
 	public String editReservation(Model model, @RequestParam("id") String reservationID) {
-		model.addAttribute("reservation", reservations.getReservationByFakeID(Long.parseLong(reservationID)));
+		Reservation r = reservations.getReservationByFakeID(Long.parseLong(reservationID));
+		model.addAttribute("reservation", r);
 		IndexController.addPages(model);
+		List<String> games = Arrays.asList(webInf.findById("games").get().getInformation().split(","));
+		model.addAttribute("games", games);
 		return "editReservation";
+	}
+	
+	@PostMapping("/reservations/edit")
+	public String editReservation(Model model, 
+			@RequestParam("id") String id,
+			@RequestParam("game") String game,
+			@RequestParam("date") String date,
+			@RequestParam("time") String time,
+			@RequestParam("who") String who) {
+		Reservation r = reservations.getOne(Integer.parseInt(id));
+		r.setGame(game);
+		r.setDate(date);
+		r.setTime(time);
+		r.setPeople(who);
+		r.setStatus(0);
+		
+		String body = "Game: " + game + "\nWho with: " + who + "\nDate: " + date + "\nTime: " + time;
+		String header = "Reservation has been updated. Here is the new information:\n";
+		String footer = "\n\nReservation id: " + r.getReservationID();
+		
+		EmailSender.sendSimpleEmail("Reservation change " + r.getReservationID(), body, "ben@zgamelogic.com", "notification@zgamelogic.com");
+		EmailSender.sendSimpleEmail("Reservation change", header + body + footer, r.getEmail(), "no-reply@zgamelogic.com");
+		
+		reservations.save(r);
+		
+		IndexController.addPages(model);
+		return "response";
+	}
+	
+	@PostMapping("/reservations/delete")
+	public String deleteReservation(Model model, 
+			@RequestParam("id") String id) {
+		Reservation r = reservations.getOne(Integer.parseInt(id));
+		reservations.delete(r);
+		
+		String body = "Reservation has been deleted\n\nReservationID: " + r.getReservationID();
+		
+		EmailSender.sendSimpleEmail("Reservation deleted " + r.getReservationID(), "A reservation was deleted", "ben@zgamelogic.com", "notification@zgamelogic.com");
+		EmailSender.sendSimpleEmail("Reservation deleted", body, r.getEmail(), "no-reply@zgamelogic.com");
+		
+		IndexController.addPages(model);
+		return "response";
 	}
 	
 	@PostMapping("/reservations")
--- src/main/java/dataStructures/database/reservation/Reservation.java
+++ src/main/java/dataStructures/database/reservation/Reservation.java
@@ -9,9 +9,11 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
