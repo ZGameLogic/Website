077e41ad40371bb25ac2711e73ff1239fce786a2
BShabowski <benshabowski@gmail.com>
2023-06-19 19:57:39 -0500

    Updating HealthCheck.java

--- src/main/java/controllers/API/HealthCheck.java
+++ src/main/java/controllers/API/HealthCheck.java
@@ -1,14 +1,15 @@
 package controllers.API;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("api/health")
 public class HealthCheck {
     @GetMapping
-    public String health(){
-        return "Healthy";
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok("Healthy");
     }
 }
